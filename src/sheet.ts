17f271f703f7e76d385c2a90ad79a6eb1ef9a85a
sugiii8 <t.sugie.1000@gmail.com>
2022-07-05 18:58:37 +0900

    sheet処理

---
+++ src/sheet.ts
@@ -0,0 +1,56 @@
+const SHEET_ID_MEMBERS = ""; // TODO ENV
+const SHEET_ID_TOPIC = ""; // TODO ENV
+const SHEET_DATA_RANGE = "A:A";
+const TEAM_NUMBER = 2; // 現在２チームに分かれて朝回
+
+type members = Array<string>;
+type membersOfEachTeam = {
+  a: members;
+  b: members;
+};
+
+// memberをマスタシートから取得
+const getDataFromSheet = (sheetId: string, range: string) => {
+  const sheet = SpreadsheetApp.openById(sheetId);
+  const data = sheet.getRange(range).getValues().filter(String).flat();
+  return data;
+};
+
+const getTopicAtRandom = () => {
+  const topics = getDataFromSheet(SHEET_ID_TOPIC, SHEET_DATA_RANGE);
+  const topic = topics[Math.floor(Math.random() * topics.length)];
+
+  return topic;
+};
+
+const getMembersOfEachTeam = () => {
+  const members = getDataFromSheet(SHEET_ID_MEMBERS, SHEET_DATA_RANGE);
+  shuffle(members);
+  const membersEachTeam = splitByTeam(members);
+
+  return membersEachTeam;
+};
+
+// 破壊的シャッフル
+const shuffle = (members: members) => {
+  for (var i = shuffle.length - 1; i > 0; i--) {
+    var r = Math.floor(Math.random() * (i + 1));
+    var tmp = shuffle[i];
+    shuffle[i] = shuffle[r];
+    shuffle[r] = tmp;
+  }
+};
+
+const splitByTeam = (members: members): membersOfEachTeam => {
+  const half = Math.ceil(members.length / TEAM_NUMBER);
+  const a = members.splice(0, half);
+  const b = members.splice(-half);
+  const membersOfEachTeam = {
+    a: a,
+    b: b,
+  };
+
+  return membersOfEachTeam;
+};
+
+export { getMembersOfEachTeam, getTopicAtRandom };
